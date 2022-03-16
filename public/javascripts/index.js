b27af872328f179533f449474b3ce83a76676926
jhsm croonen <info@jcroonen.nl>
2022-03-16 11:46:08 +0100

    removed time field, as that is tackled in youtubetime chrome extension

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,88 +1,107 @@
 const plaintext = "text/plain";
 const htmltext = "text/html";
 
-function getTime(seconds) {
-    const minutes = Math.floor(seconds / 60);
-    seconds = seconds % 60;
-    if (seconds < 10) {
-        seconds = ' ' + seconds;
+const buttonReadClipboard = document.getElementById('buttonReadClipboard');
+const buttonCopyHtml = document.getElementById('buttonCopyHtml');
+const buttonReplace = document.getElementById('buttonReplace');
+const buttonCopy = document.getElementById('buttonCopy');
+const aTimetable = document.getElementById('aTimetable');
+
+const inputHtml = document.getElementById('inputHtml');
+const inputText = document.getElementById('inputText');
+const inputMatch = document.getElementById('inputMatch');
+const inputTarget = document.getElementById('inputTarget');
+
+const msgHtmlCopied = document.getElementById('msgHtmlCopied');
+const msgAllCopied = document.getElementById('msgAllCopied');
+const msgReplaced = document.getElementById('msgReplaced');
+
+function bind() {
+    [
+        [buttonReadClipboard, readClipboard],
+        [buttonCopyHtml, copyHtml],
+        [buttonCopy, copyAll],
+        [buttonReplace, replace],
+        [aTimetable, youtubeTimetable]
+    ].forEach(binding => {
+        const [element, listener] = binding;
+        element.addEventListener('click', listener);
+    })
+}
+
+function hideMsgs() {
+    msgHtmlCopied.style.visibility = 'hidden';
+    msgAllCopied.style.visibility = 'hidden';
+    msgReplaced.style.visibility = 'hidden';
+}
+
+function youtubeTimetable() {
+    inputMatch.value = '<a ';
+    inputTarget.value = '<br><a ';
+}
+
+function replace() {
+    if (inputHtml.value.length < 1) {
+        return;
     }
-    return `${minutes}:${seconds}`;
+    inputHtml.value = inputHtml.value
+        .split(inputMatch.value)
+        .join(inputTarget.value);
+    hideMsgs();
+    msgReplaced.style.visibility = 'visible';
 }
 
-function showText(text) {
-    const pos = text.indexOf('?t=');
-    if (pos !== -1) {
-        document.getElementById('clipboard-text').value = text.substr(0, pos);
-        document.getElementById('clipboard-time').value = getTime(text.substr(pos+3));
-    } else {
-        document.getElementById('clipboard-text').value = text;
+function copyAll() {
+    if (inputHtml.value.length < 1 && inputText.value.length < 1) {
+        return;
     }
-}
-
-function showHtml(text) {
-    document.getElementById('clipboard-html').value = text;
-}
-
-function getSeconden(time) {
-    const parts = time.split(':');
-    let seconden;
-    if (parts.length > 2) {
-        seconden = parts[0] * 60 * 60;
-        seconden += parseInt(parts[1] * 60);
-        seconden += parseInt(parts[2]);
-    } else if (parts.length > 1) {
-        seconden = parseInt(parts[0] * 60);
-        seconden += parseInt(parts[1]);
-    } else {
-        seconden = parts[0];
-    }
-    return seconden;
-}
-
-function writeHtml() {
-    const text = document.getElementById('clipboard-text').value;
-    const time = document.getElementById('clipboard-time').value;
-    const seconden = getSeconden(time);
-    const qs = seconden ? `&t=${seconden}` : '';
-    document.getElementById('clipboard-html').value  = `
-<p style="margin:0in;font-family:Calibri;font-size:12.0pt" lang="nl">
-<!--StartFragment--><a href="${text}${qs}">${time}</a><!--EndFragment--></p>`;
-    document.getElementById('clipboard-url').setAttribute('href', text + qs);
-}
-
-function adjustTimeTable() {
-    document.getElementById('clipboard-html').value =
-        document.getElementById('clipboard-html').value
-            .replace(/<a /g, '<br><a ');
+    const htmlBlob = new Blob([inputHtml.value], {type: htmltext});
+    const textBlob = new Blob([inputText.value], {type: plaintext});
+    const item = new ClipboardItem({
+        [htmltext]: htmlBlob,
+        [plaintext]: textBlob
+    });
+    navigator.clipboard.write([item]).then(
+        () => {
+            hideMsgs();
+            msgAllCopied.style.visibility = 'visible';
+        }
+    );
 }
 
 function copyHtml() {
-    const htmlElement = document.getElementById('clipboard-html');
-    const blob = new Blob([htmlElement.value], {type: htmltext});
-    const item = new ClipboardItem({
-        [htmltext]: blob,
-    });
-    navigator.clipboard.write([item]).then();
+    if (inputHtml.value.length < 1) {
+        return;
+    }
+    const blob = new Blob([inputHtml.value], {type: htmltext});
+    const item = new ClipboardItem({[htmltext]: blob,});
+    navigator.clipboard.write([item]).then(
+        () => {
+            hideMsgs();
+            msgHtmlCopied.style.visibility = 'visible';
+        }
+    );
 }
 
 function showPlainText(item) {
     item.getType(plaintext).then((blob) => {  // (blob: Blob)
         new Response(blob).text().then(text => {
-            showText(text);
+            inputText.value = text;
         })
     })
 }
 
+
 function showHtmlText(item) {
     item.getType(htmltext).then(blob => {
         new Response(blob).text().then(text => {
-            showHtml(text);
+            inputHtml.value = text;
         })
     })
 }
 
 function showItem(item) {
+    // todo: should only process first (or, only) item
     for (let j = 0; j < item.types.length; j++) {
         switch(item.types[j]) {
             case plaintext:
@@ -101,3 +120,6 @@ function readClipboard() {
         }
     })
 }
+
+bind();
+hideMsgs();
